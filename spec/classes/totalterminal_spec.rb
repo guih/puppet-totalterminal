cfac5fa27659473a8f1c78df69eb801adb92e2e1
Guilherme Matsumoto <guih.matsumoto@gmail.com>
2014-10-19 12:46:23 -0200

    Update totalterminal_spec.rb to v1.5.3

--- spec/classes/totalterminal_spec.rb
+++ spec/classes/totalterminal_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'totalterminal' do
   it do
-    should contain_package('TotalTerminal 1.3').with({
-      :source   => 'http://downloads-1.binaryage.com/TotalTerminal-1.3.dmg',
+    should contain_package('TotalTerminal 1.5.3').with({
+      :source   => 'http://downloads-1.binaryage.com/TotalTerminal-1.5.3.dmg',
       :provider => 'pkgdmg'
     })
   end
