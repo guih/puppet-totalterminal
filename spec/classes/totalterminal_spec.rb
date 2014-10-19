49a6fd0becb66a1abadbe5a0d46f6429ddaa260d
Guilherme Matsumoto <guih.matsumoto@gmail.com>
2014-10-19 15:43:50 -0200

    Update base download url for spec

--- spec/classes/totalterminal_spec.rb
+++ spec/classes/totalterminal_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'totalterminal' do
   it do
     should contain_package('TotalTerminal 1.5.3').with({
-      :source   => 'http://downloads-1.binaryage.com/TotalTerminal-1.5.3.dmg',
+      :source   => 'http://downloads.binaryage.com/TotalTerminal-1.5.3.dmg',
       :provider => 'pkgdmg'
     })
   end
