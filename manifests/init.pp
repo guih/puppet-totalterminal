66e34dcc5a5964c8bd4e2df81d03286d72e221ac
Guilherme Matsumoto <guih.matsumoto@gmail.com>
2014-10-19 15:24:17 -0200

    Parametrized version

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,9 @@
 # Examples
 #
 #   include totalterminal
-class totalterminal {
-  package { 'TotalTerminal 1.3':
+class totalterminal($version='1.3') {
+  package { 'TotalTerminal ${version}':
     provider => 'pkgdmg',
-    source   => 'http://downloads-1.binaryage.com/TotalTerminal-1.3.dmg'
+    source   => 'http://downloads-1.binaryage.com/TotalTerminal-${version}.dmg'
   }
 }
